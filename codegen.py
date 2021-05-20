d67678f163727ddf1b37b013a47ab1649ee1fe07
adrianschneider94 <post@adrian-schneider.de>
2021-05-20 17:20:50 +0200

    Added patch for wrong OpenAPI types (VariableValueDto)

--- codegen.py
+++ codegen.py
@@ -1,10 +1,31 @@
-import urllib.parse
+import json
 from pathlib import Path
 
+import jsonpatch
+import requests
 from datamodel_code_generator import InputFileType, generate
 
+# Retrieve OpenAPI schema
+api = json.loads(requests.get("https://start.camunda.com/openapi.json").text)
+
+# Apply patches
+patches = [
+    {
+        'op':   'remove',
+        'path': '/components/schemas/VariableValueDto/properties/value/type',
+    },
+    {
+        'op':   'remove',
+        'path': '/components/schemas/VariableValueDto/properties/valueInfo/type',
+    }
+]
+
+patch = jsonpatch.JsonPatch(patches)
+patched_api = patch.apply(api)
+
+# Generate pydantic models
 generate(
-    input_=urllib.parse.urlparse("https://start.camunda.com/openapi.json"),
+    input_=json.dumps(patched_api),
     input_file_type=InputFileType.OpenAPI,
     output=Path("./src/camunda/models.py"),
     allow_population_by_field_name=True,
