aecfdf677bd27428cf96060f4f940f793aa8b304
adrianschneider <post@adrian-schneider.de>
2021-05-21 19:24:38 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='camunda',
+    version='0.0.1',
+    packages=find_packages("src"),
+    package_dir={
+        'camunda': "./src/camunda",
+    },
+    url='',
+    license='',
+    author='Adrian Schneider',
+    author_email='adrian.schneider@sangl.com',
+    description='',
+    install_requires=['requests', 'pydantic'],
+    extras_require={
+        'dev': [
+            'jsonpatch',
+            'datamodel-code-generator'
+        ]
+    }
+
+)
